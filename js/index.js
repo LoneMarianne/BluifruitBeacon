8cb3e9e11416e28c159d816e3c25e873b1beeb4d
LoneMarianne <loneoesterlund@gmail.com>
2019-09-08 19:16:37 +0200

    diverse

--- js/index.js
+++ js/index.js
@@ -1,4 +1,53 @@
 //https://www.tutorialspoint.com/cordova/cordova_inappbrowser.htm
+
+/*var blue= {
+    serviceUUID: "0000FFE0-0000-1000-8000-00805F9B34FB",
+    characteristicUUID: "0000FFE1-0000-1000-8000-00805F9B34FB"
+};*/
+
+//the bluefruit UART Service
+var blue ={
+	serviceUUID: '6e400001-b5a3-f393-e0a9-e50e24dcca9e',
+    txCharacteristic: '6e400002-b5a3-f393-e0a9-e50e24dcca9e', // transmit is from the phone's perspective
+    rxCharacteristic: '6e400003-b5a3-f393-e0a9-e50e24dcca9e'  // receive is from the phone's perspective
+}
+
+var ConnDeviceId;
+var deviceList =[];
+ 
+function onLoad(){
+	document.addEventListener('deviceready', onDeviceReady, false);
+  bleDeviceList.addEventListener('touchstart', conn, false); // assume not scrolling
+}
+
+function onDeviceReady(){
+	refreshDeviceList();
+}
+
+	 
+function refreshDeviceList(){
+	//deviceList =[];
+	document.getElementById("bleDeviceList").innerHTML = ''; // empties the list
+	if (cordova.platformId === 'android') { // Android filtering is broken
+		ble.scan([], 5, onDiscoverDevice, onError);
+	} else {
+		//alert("Disconnected");
+		ble.scan([blue.serviceUUID], 5, onDiscoverDevice, onError);
+	}
+}
+
+
+function onDiscoverDevice(device){
+	//Make a list in html and show devises
+	 if(device.name =="TESTLONE"){
+		var listItem = document.createElement('li'),
+		html = device.name+ "," + device.id;
+		listItem.innerHTML = html;
+		document.getElementById("bleDeviceList").appendChild(listItem);
+		openBrowser('https://placering.000webhostapp.com');
+	 }
+}
+
 function test(){
 	var url='https://placering.000webhostapp.com';
 	
