ad74f28c408156aa6da76ff296dfcb5a8c620dec
LoneMarianne <loneoesterlund@gmail.com>
2019-09-08 18:09:02 +0200

    egen testside

--- js/index.js
+++ js/index.js
@@ -1,16 +1,14 @@
 //https://www.tutorialspoint.com/cordova/cordova_inappbrowser.htm
 function test(){
-	var url='https://cordova.apache.org';
-	var number = urlInput.value;
-	if(number == 1) url ='http://www.kea.dk';
-	if(number == 2) url ='https://www.dr.dk/';
+	var url='https://placering.000webhostapp.com';
+	
 	openBrowser(url);
 }
 
 function openBrowser(url) {
    //var url = 'https://cordova.apache.org';
    var target = '_blank';
-   var options = "location = yes"
+   var options = "location = no"
    var ref = cordova.InAppBrowser.open(url, target, options);
 }
 
